91a632ecf2d8fa84ecbc131be4096871464b6d51
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-04-02 00:32:24 +0900

    書籍検索・登録機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,53 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.includes(:user)
   end
 
   def new
+    @books = []
+    @title = params[:title]
+    if @title.present?
+      results = RakutenWebService::Books::Book.search({
+        title: @title
+      })
+
+      results.each do |result|
+        book = Book.new(read(result))
+        @books << book
+      end
+    end
   end
 
   def create
+    @book = Book.find_or_initialize_by(isbn: params[:isbn])
+    unless @book.persisted?
+      result = RakutenWebService::Books::Book.search(isbn: @book.isbn)
+      @book = Book.new(read(result.first))
+      if @book.save
+        redirect_to root_path
+      else
+        render action: :new
+      end
+    end
   end
+
+  private
+  def read(result)
+    isbn = result["isbn"]
+    title = result["title"]
+    author = result["author"]
+    caption = result["itemCaption"]
+    image_url = result["mediumImageUrl"].gsub('?_ex=120x120', '')
+    url = result["itemUrl"]
+    {
+      isbn: isbn,
+      title: title,
+      author: author,
+      caption: caption,
+      image_url: image_url,
+      url: url,
+      user_id: current_user.id
+    }
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
+  has_many :books, primary_key: "isbn"
 end
